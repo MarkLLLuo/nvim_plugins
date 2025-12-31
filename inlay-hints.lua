e2cde4f4a68d814d8e7d52944409b72199deb279
LLL <luoruoyus@gmail.com>
2026-01-01 02:19:13 +0800

    fix(inlay-hints.lua): change gloval inlay hints setting to false

--- inlay-hints.lua
+++ inlay-hints.lua
@@ -3,11 +3,14 @@ return {
         event = "LspAttach",
         dependencies = { "neovim/nvim-lspconfig" }, -- optional
         config = function()
-                require("inlay-hints").setup()
+                require("inlay-hints").setup {
+                        commands = { enable = true }, -- Enable commands: InlayHintsToggle, InlayHintsEnable, InlayHintsDisable
+                        autocmd = { enable = false }, -- Auto-enable inlay hints on LspAttach
+                }
 
                 vim.keymap.set("n", "<leader>i", function()
                         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
                         vim.notify(vim.lsp.inlay_hint.is_enabled() and "Inlay Hints Enabled" or "Inlay Hints Disabled")
-                end, { desc = "inlay hints" })
+                end, { desc = "Toggle inlay hints" })
         end,
 }
