1cfcb220c91d563108767d46444cebf7157e4dc5
LLL <luoruoyus@gmail.com>
2025-12-31 19:10:37 +0800

    fix: keymap conflict for inlay hints

--- inlay-hints.lua
+++ inlay-hints.lua
@@ -1,8 +1,13 @@
 return {
-        {
-                "MysticalDevil/inlay-hints.nvim",
-                event = "LspAttach",
-                dependencies = { "neovim/nvim-lspconfig" }, -- optional
-                config = function() require("inlay-hints").setup() end,
-        },
+        "MysticalDevil/inlay-hints.nvim",
+        event = "LspAttach",
+        dependencies = { "neovim/nvim-lspconfig" }, -- optional
+        config = function()
+                require("inlay-hints").setup()
+
+                vim.keymap.set("n", "<leader>i", function()
+                        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+                        vim.notify(vim.lsp.inlay_hint.is_enabled() and "Inlay Hints Enabled" or "Inlay Hints Disabled")
+                end, { desc = "inlay hints" })
+        end,
 }
--- keymaps.lua
+++ keymaps.lua
@@ -1,4 +1,5 @@
-vim.keymap.set("n", "<leader>h", function()
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+vim.keymap.set("n", "<leader>i", function()
         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
         vim.notify(vim.lsp.inlay_hint.is_enabled() and "Inlay Hints Enabled" or "Inlay Hints Disabled")
 end)
