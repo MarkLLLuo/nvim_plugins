9452886b6934b44eaa2e80504fb5def767f2d6ea
LLL <luoruoyus@gmail.com>
2026-08-09 02:58:16 +0800

    add(fff.lua): add file search plugin

--- catppucci.lua
+++ catppucci.lua
@@ -1,18 +1,18 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
-  -- Define the plugin using lazy.nvim format
-  "catppuccin/nvim",
-  name = "catppuccin",
-  config = function()
-    -- Plugin setup according to catppuccin documentation
-    require("catppuccin").setup({
-      flavour = "frappe", -- Options: "latte", "frappe", "macchiato", "mocha"
-      background = {
-        light = "latte",
-        dark = "macchiato",
-      },
-    })
-    -- Set the colorscheme
-    vim.cmd("colorscheme catppuccin")
-  end,
+        -- Define the plugin using lazy.nvim format
+        "catppuccin/nvim",
+        name = "catppuccin",
+        config = function()
+                -- Plugin setup according to catppuccin documentation
+                require("catppuccin").setup {
+                        flavour = "frappe", -- Options: "latte", "frappe", "macchiato", "mocha"
+                        background = {
+                                light = "latte",
+                                dark = "macchiato",
+                        },
+                }
+                -- Set the colorscheme
+                vim.cmd "colorscheme catppuccin"
+        end,
 }
-
